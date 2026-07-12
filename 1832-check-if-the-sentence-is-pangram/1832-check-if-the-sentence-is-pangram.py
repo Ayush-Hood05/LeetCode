2586709817f827a5f4a057b8ffffcdf16f4ba756
Ayush Hood <ayushhood20@gmail.com>
2026-07-12 20:01:15 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0.42%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def checkIfPangram(self, sentence):
+        # edge case 
+        if len(sentence) < 26:
+            return False 
+
+        for i in range(26): 
+            if chr(97 + i) not in sentence:
+                return False 
+                
+        return True
+        
