e03c7703ae641c1e5cf0c0a9d9a55902c2e04b20
Ayush Hood <ayushhood20@gmail.com>
2026-08-05 21:44:13 +0530

    Time: 5093 ms (5%), Space: 13.2 MB (36.15%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range (len(nums)):
+            for j in range(len(nums)):
+                if i != j and nums[i] + nums[j] == target :
+                    return [i,j]
+
+        return [-1,-1]
+        
