cfd278409e75ce4bb6faa35425469ec6b353b519
Ayush Hood <ayushhood20@gmail.com>
2026-08-05 22:25:14 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,13 +1,21 @@
 class Solution(object):
     def sortColors(self, nums):
-        n = len(nums)
-        # Selection Sort 
-        for i in range (n):
-            min_idx = i
-            for j in range (i,n):
-                if nums[min_idx] > nums [j]:
-                    min_idx = j
+        # this question has only 0,1,2 keys therefore 
+        low = 0 # for 0's
+        mid = 0 # for 1's
+        high = len(nums) - 1 # for 2's
 
-            # swap 
-            nums[min_idx] , nums [i] = nums[i] , nums[min_idx]
+        while mid <= high : 
+            if nums[mid] == 0:
+                nums[low] , nums[mid] = nums[mid] , nums[low]
+                mid += 1
+                low += 1
+
+            elif nums[mid] == 1:
+                nums[mid] = 1
+                mid += 1
+        
+            else : 
+                nums[mid] , nums[high] = nums[high] , nums[mid]
+                high -= 1
         
