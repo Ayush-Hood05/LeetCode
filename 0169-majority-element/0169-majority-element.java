e0f01d7a44b134b10ea0d8518f11a589c17e1695
Ayush Hood <ayushhood20@gmail.com>
2026-09-01 00:42:28 +0530

    Time: 6 ms (51.53%), Space: 55.3 MB (69.1%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //Moore Voting Algorithm
-        int count = 0;
-        int occur = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                occur = num;
-            }
-            if (num == occur) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-        return occur;
+        // sort
+        Arrays.sort(nums);
+        int mid = nums.length/2;
+        return nums[mid];
     }
 }
